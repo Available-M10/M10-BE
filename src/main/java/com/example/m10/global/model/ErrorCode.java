d4f8fc075053f8edff32609a23de55f8a74dc9f2
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-08-28 10:15:29 +0900

    fix(#3): 잘못 표기된 문법 수정

--- src/main/java/com/example/m10/global/model/ErrorCode.java
+++ src/main/java/com/example/m10/global/model/ErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류 입니다."),
 
     EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "로그인 세션이 만료되었습니다."),
-    INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않는 토큰입니다.");
+    INVALID_JWT(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
