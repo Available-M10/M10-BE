8109b17f72863dd48204ba313924f256ddf2a78e
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-01 09:50:31 +0900

    feat(#5): signup service 개발

---
+++ src/main/java/com/example/m10/domain/auth/service/SignupService.java
@@ -0,0 +1,29 @@
+package com.example.m10.domain.auth.service;
+
+import com.example.m10.domain.auth.dto.request.SignupRequestDto;
+import com.example.m10.domain.user.domain.User;
+import com.example.m10.domain.user.domain.repository.UserRepository;
+import com.example.m10.domain.user.exception.UserExistException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SignupService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(SignupRequestDto dto){
+        if(userRepository.findByAccountId(dto.accountId()).isPresent()){
+            throw new UserExistException();
+        }
+
+        userRepository.save(User.builder()
+                .accountId(dto.accountId())
+                .password(passwordEncoder.encode(dto.password()))
+                .build());
+    }
+}
