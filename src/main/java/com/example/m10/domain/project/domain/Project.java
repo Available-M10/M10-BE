677ba9aeb6e016a2a5f4ab7d332d7d994b884c18
sorim0626 <enhld00@gmail.com>
2025-09-04 18:52:09 +0900

    refactor(#7): node 관계 orphanRemoval 적용

--- src/main/java/com/example/m10/domain/project/domain/Project.java
+++ src/main/java/com/example/m10/domain/project/domain/Project.java
@@ -20,6 +20,7 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     private boolean active; // 현재는 on / off 두 개만 있어서 boolean으로 처리했는데 나중에 상태가 더 늘어나면 enum으로 바꾸는게 좋을듯
@@ -29,7 +30,11 @@ public class Project {
     private LocalDateTime updateAt;
 
     @Builder.Default
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(
+            mappedBy = "project",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true)
     private List<Node> nodes = new ArrayList<>();
 
     public void updateName(String name) {
