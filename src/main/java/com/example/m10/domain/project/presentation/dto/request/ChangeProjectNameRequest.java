284351e57285f1950738fa454b92aea4ce37cd71
sorim0626 <enhld00@gmail.com>
2025-09-04 18:53:25 +0900

    chore(#7): NotBlank 적용

--- src/main/java/com/example/m10/domain/project/presentation/dto/request/ChangeProjectNameRequest.java
+++ src/main/java/com/example/m10/domain/project/presentation/dto/request/ChangeProjectNameRequest.java
@@ -1,6 +1,9 @@
 package com.example.m10.domain.project.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChangeProjectNameRequest(
+        @NotBlank
         String name
 ) {
 }
