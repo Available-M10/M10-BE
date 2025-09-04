60bcc05881608f6190d3b7f37844170fc8c6d1e0
sorim0626 <enhld00@gmail.com>
2025-09-04 17:59:51 +0900

    feat(#7): Project 엔티티 추가

---
+++ src/main/java/com/example/m10/domain/project/domain/Project.java
@@ -0,0 +1,39 @@
+package com.example.m10.domain.project.domain;
+
+import com.example.m10.domain.node.domain.Node;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean active; // 현재는 on / off 두 개만 있어서 boolean으로 처리했는데 나중에 상태가 더 늘어나면 enum으로 바꾸는게 좋을듯
+    // 아니면 관리자같은 권한이 추가되면 enum으로 바꾸거나
+
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    @Builder.Default
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Node> nodes = new ArrayList<>();
+
+    public void updateName(String name) {
+        this.name = name;
+        this.updateAt = LocalDateTime.now();
+    }
+}
