7254104750c30d42a3d159eef2a882e10462beea
sorim0626 <enhld00@gmail.com>
2025-09-04 18:11:22 +0900

    feat(#7): 프로젝트 전체 조회 서비스

---
+++ src/main/java/com/example/m10/domain/project/service/QueryAllProjectService.java
@@ -0,0 +1,24 @@
+package com.example.m10.domain.project.service;
+
+import com.example.m10.domain.project.domain.repository.ProjectRepository;
+import com.example.m10.domain.project.presentation.dto.response.QueryAllProjectResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAllProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Transactional(readOnly = true)
+    public List<QueryAllProjectResponse> getAllProjects() {
+        return projectRepository.findAll()
+                .stream()
+                .map(QueryAllProjectResponse::from)
+                .toList();
+    }
+}
