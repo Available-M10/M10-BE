c7ddfb60d232ef36a93d2348999b923cf12a9a05
sorim0626 <enhld00@gmail.com>
2025-09-04 18:10:52 +0900

    feat(#7): 프로젝트 생성 서비스

---
+++ src/main/java/com/example/m10/domain/project/service/CreateProjectService.java
@@ -0,0 +1,37 @@
+package com.example.m10.domain.project.service;
+
+import com.example.m10.domain.project.domain.Project;
+import com.example.m10.domain.project.domain.repository.ProjectRepository;
+import com.example.m10.domain.project.presentation.dto.request.CreateProjectRequest;
+import com.example.m10.domain.project.presentation.dto.response.ProjectResponse;
+import com.example.m10.domain.user.domain.User;
+import com.example.m10.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class CreateProjectService {
+
+    private final UserFacade userFacade;
+    private final ProjectRepository projectRepository;
+
+    @Transactional
+    public ProjectResponse createProject(CreateProjectRequest request) {
+        User user = userFacade.currentUser();
+
+        Project project = projectRepository.save(
+                Project.builder()
+                        .name(request.name())
+                        .active(false)
+                        .createAt(LocalDateTime.now())
+                        .updateAt(LocalDateTime.now())
+                        .build()
+        );
+
+        return ProjectResponse.from(project);
+    }
+}
