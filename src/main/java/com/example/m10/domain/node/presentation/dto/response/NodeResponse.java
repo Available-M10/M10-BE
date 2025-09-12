d5252feb46f72afc1759375bc0ad0887339e6168
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-12 23:45:20 +0800

    feat(#9): node response 구현

--- src/main/java/com/example/m10/domain/node/presentation/dto/response/NodeResponse.java
+++ src/main/java/com/example/m10/domain/node/presentation/dto/response/NodeResponse.java
@@ -1,15 +1,31 @@
 package com.example.m10.domain.node.presentation.dto.response;
 
-import com.example.m10.domain.node.domain.Node;
+import com.example.m10.domain.node.domain.entity.common.Node;
 
 public record NodeResponse(
-        Long id,
-        String name
+        Long nodeId,
+        String name,
+        String type,
+        String configJson,
+        String myPortId
 ) {
-    public static NodeResponse from(Node node) {
+    public static NodeResponse form(Node node, String myPortId){
         return new NodeResponse(
                 node.getId(),
-                node.getName()
+                node.getName().toString(),
+                node.getType().toString(),
+                node.getConfigJson(),
+                myPortId
+        );
+    }
+
+    public static NodeResponse form(Node node){
+        return new NodeResponse(
+                node.getId(),
+                node.getName().toString(),
+                node.getType().toString(),
+                node.getConfigJson(),
+                "LAST NODE"
         );
     }
 }
