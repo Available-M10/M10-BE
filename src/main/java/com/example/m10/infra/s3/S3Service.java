269970de5836a29e8add3d17832b5bcf6acfe788
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-12 23:34:32 +0800

    feat(#9): s3 관련 로직 구현

---
+++ src/main/java/com/example/m10/infra/s3/S3Service.java
@@ -0,0 +1,68 @@
+package com.example.m10.infra.s3;
+
+import com.example.m10.infra.s3.exception.InternalS3Error;
+import com.example.m10.infra.s3.exception.ObjectKeyNotFound;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+    private final S3Properties s3Properties;
+
+    public URL createUploadPresignedUrl(String objectKey, Duration expiration) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(s3Properties.bucket())
+                .key(objectKey)
+                .build();
+
+        PutObjectPresignRequest presignedRequest = PutObjectPresignRequest.builder()
+                .putObjectRequest(request)
+                .signatureDuration(expiration)
+                .build();
+
+        return s3Presigner.presignPutObject(presignedRequest).url();
+    }
+
+    private void validateObjectExists(String objectKey) {
+        try {
+            s3Client.headObject(
+                    req -> req
+                            .bucket(s3Properties.bucket())
+                            .key(objectKey)
+            );
+        } catch (NoSuchKeyException e) {
+            throw new ObjectKeyNotFound();
+        } catch (S3Exception e) {
+            throw new InternalS3Error();
+        }
+    }
+
+    private String encodeObjectKey(String objectKey) {
+        return Arrays.stream(objectKey.split("/"))
+                .map(part -> URLEncoder.encode(part, StandardCharsets.UTF_8))
+                .collect(Collectors.joining("/"));
+    }
+
+    public String generateUrl(String objectKey) {
+        validateObjectExists(objectKey);
+        String encodedKey = encodeObjectKey(objectKey);
+        return "https://" + s3Properties.bucket() + ".s3." + s3Properties.region() + ".amazonaws.com/" + encodedKey;
+    }
+}
