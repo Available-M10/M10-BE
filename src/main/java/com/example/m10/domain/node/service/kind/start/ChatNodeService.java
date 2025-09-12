288ed9fe1cc55a9f5b2019c8ca5b587099294a30
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-13 00:18:49 +0900

    feat(#9): 채팅 노드 서비스 로직 구현

---
+++ src/main/java/com/example/m10/domain/node/service/kind/start/ChatNodeService.java
@@ -0,0 +1,20 @@
+package com.example.m10.domain.node.service.kind.start;
+
+import com.example.m10.domain.node.presentation.dto.response.NodeResponse;
+import com.example.m10.domain.node.service.common.CommonStartNode;
+import com.example.m10.domain.node.service.common.define.CommonTriggerNode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatNodeService implements CommonTriggerNode {
+    private final CommonStartNode startNode;
+
+    @Override
+    @Transactional
+    public NodeResponse createNode(Long projectId){
+        return startNode.createNode(projectId);
+    }
+}
