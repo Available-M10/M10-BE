f779808ed81d5faa881d7de646f9c99d6c3642ee
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-13 00:24:51 +0900

    feat(#9): 공통 노드의 interface 구현

--- src/main/java/com/example/m10/domain/node/service/common/define/CommonNode.java
+++ src/main/java/com/example/m10/domain/node/service/common/define/CommonNode.java
@@ -5,3 +5,5 @@ import com.example.m10.domain.node.presentation.dto.response.NodeResponse;
 public interface CommonNode {
     NodeResponse createNode(Long projectId, String previousPortId, String json);
 }
+
+
