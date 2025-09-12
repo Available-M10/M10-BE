ccb324838dc59f37a396a916b862ba64759d0a55
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-13 00:27:30 +0900

    feat(#9): 문서 노드 서비스 로직 구현

---
+++ src/main/java/com/example/m10/domain/node/service/kind/middle/DocumentNodeService.java
@@ -0,0 +1,49 @@
+package com.example.m10.domain.node.service.kind.middle;
+
+import com.example.m10.domain.node.exception.DtoConversionException;
+import com.example.m10.domain.node.presentation.dto.request.DocumentNodeRequestDto;
+import com.example.m10.domain.node.presentation.dto.response.NodeResponse;
+import com.example.m10.domain.node.service.common.CommonMiddleNode;
+import com.example.m10.domain.node.service.common.define.CommonNode;
+import com.example.m10.domain.node.service.common.define.CommonRunNode;
+import com.example.m10.domain.node.service.common.define.NodeOutput;
+import com.example.m10.domain.node.service.common.define.RunContext;
+import com.example.m10.infra.s3.S3Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class DocumentNodeService implements CommonNode, CommonRunNode {
+    private final CommonMiddleNode middleNode;
+    private final S3Service s3Service;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    @Transactional
+    public NodeResponse createNode(Long projectId, String previousPortId, String json){
+        return middleNode.createNode(projectId, previousPortId, json);
+    }
+
+    @Override
+    @Transactional
+    public NodeOutput run(RunContext ctx) {
+        String json = ctx.node().getConfigJson();
+        DocumentNodeRequestDto dto;
+        try{
+            dto = objectMapper.readValue(json, DocumentNodeRequestDto.class);
+        } catch (JsonProcessingException e){
+            throw new DtoConversionException();
+        }
+
+        //TODO: AI와 연결 후 Embedding Model로 Embedding 한 후 VectorDB에 저장 로직 구현
+        String fileUrl = s3Service.generateUrl(dto.objectKey());
+        Long projectId = ctx.projectId();
+        System.out.println("project id: "+projectId+", file url: "+fileUrl +", EmbeddingModel: " + dto.embeddingModel()+", VectorDB: "+dto.vectorDB());
+        return NodeOutput.none();
+    }
+}
