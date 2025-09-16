ce59024bfe94db7cc6a6a0da4f23ce9d7c0db8d2
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-16 16:00:48 +0900

    refactor(#9): null 검증 로직 추가

--- src/main/java/com/example/m10/domain/node/presentation/dto/request/ChatRequestDto.java
+++ src/main/java/com/example/m10/domain/node/presentation/dto/request/ChatRequestDto.java
@@ -1,6 +1,9 @@
 package com.example.m10.domain.node.presentation.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ChatRequestDto(
+        @NotNull(message = "메세지를 입력해주세요")
         String message
 ) {
 }
