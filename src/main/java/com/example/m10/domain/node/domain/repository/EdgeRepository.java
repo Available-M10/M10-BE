92e7b8b9f43f0a298aa31e71a590deccb7db22c2
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-16 16:03:17 +0900

    refactor(#9): 메서드 추가

--- src/main/java/com/example/m10/domain/node/domain/repository/EdgeRepository.java
+++ src/main/java/com/example/m10/domain/node/domain/repository/EdgeRepository.java
@@ -3,12 +3,18 @@ package com.example.m10.domain.node.domain.repository;
 import com.example.m10.domain.node.domain.entity.common.Edge;
 import com.example.m10.domain.node.domain.entity.common.Port;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
     boolean existsByFromPortAndToPort(Port prevOutPort, Port currentPort);
     Optional<Edge> findByFromPort(Port fromPort);
+    boolean existsByFromPort_OutPortId(UUID outPortId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    void deleteByToPort_Node_Id(Long nodeId);
 }
