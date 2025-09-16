84dbcafaee763cc535d7edcf4704a427192488c9
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-16 16:01:45 +0900

    refactor(#9): 메서드 명 수정 & 엣지 제거 추가

--- src/main/java/com/example/m10/domain/node/service/DeleteNodeService.java
+++ src/main/java/com/example/m10/domain/node/service/DeleteNodeService.java
@@ -1,7 +1,7 @@
 package com.example.m10.domain.node.service;
 
+import com.example.m10.domain.node.domain.repository.EdgeRepository;
 import com.example.m10.domain.node.domain.repository.NodeRepository;
-import com.example.m10.domain.node.domain.repository.PortRepository;
 import com.example.m10.domain.node.exception.NodeDeletionNotAllowedException;
 import com.example.m10.domain.node.exception.NodeNotFoundException;
 import com.example.m10.domain.node.service.pipeline.TraversalService;
@@ -12,9 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class DeleteNodeService {
-    //노드 아이디를 입력하여 삭제 진행
-    //만약 해당 노드가 자신 앞노드와 연결이 되었다면(자신의 OutPortId가 다른 노드의 InPortId 라면) 삭제 불가
     private final NodeRepository nodeRepository;
+    private final EdgeRepository edgeRepository;
     private final TraversalService traversalService;
 
     @Transactional
@@ -23,10 +22,11 @@ public class DeleteNodeService {
             throw new NodeNotFoundException();
         }
 
-        if(!traversalService.existsConnectedNode(nodeId)){
+        if(traversalService.hasConnections(nodeId)){
             throw new NodeDeletionNotAllowedException();
         }
 
+        edgeRepository.deleteByToPort_Node_Id(nodeId);
         nodeRepository.deleteById(nodeId);
     }
 }
