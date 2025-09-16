4402980ac925a6dd531d7b644b398c35dd5ed91a
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-16 16:02:58 +0900

    refactor(#9): existsBy... 체크만으로는 경쟁 상태에서 중복 간선이 생성될 수 있기에 유니크 index 추가

--- src/main/java/com/example/m10/domain/node/domain/entity/common/Edge.java
+++ src/main/java/com/example/m10/domain/node/domain/entity/common/Edge.java
@@ -10,6 +10,14 @@ import lombok.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(
+        uniqueConstraints = @UniqueConstraint(name = "uk_edge_from_to", columnNames = {"from_port_id","to_port_id"}),
+        indexes = {
+                @Index(name="ix_edge_from_port", columnList="from_port_id"),
+                @Index(name="ix_edge_to_port", columnList="to_port_id")
+        }
+)
+
 public class Edge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,11 +29,11 @@ public class Edge {
 
     // from_port_id(UUID) → Port.out_port_id(UUID)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "from_port_id", referencedColumnName = "out_port_id")
+    @JoinColumn(name = "from_port_id", referencedColumnName = "out_port_id", nullable = false)
     private Port fromPort;
 
     // to_port_id(UUID) → Port.in_port_id(UUID)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "to_port_id", referencedColumnName = "in_port_id")
+    @JoinColumn(name = "to_port_id", referencedColumnName = "in_port_id", nullable = false)
     private Port toPort;
 }
