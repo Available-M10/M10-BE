ac57a52355a713470c7908caa7449a3bc5fa2eea
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-09-16 16:04:14 +0900

    refactor(#9): 메서드 추가

--- src/main/java/com/example/m10/domain/node/domain/repository/NodeRepository.java
+++ src/main/java/com/example/m10/domain/node/domain/repository/NodeRepository.java
@@ -4,12 +4,10 @@ import com.example.m10.domain.node.domain.entity.common.Node;
 import com.example.m10.domain.node.domain.enums.node.NodeName;
 import com.example.m10.domain.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface NodeRepository extends JpaRepository<Node, Long> {
