d5e58d81f157538fb0089be1669c3d27462aee2d
sorim0626 <enhld00@gmail.com>
2025-10-15 19:28:18 +0900

    feat: 프로젝트 전체 조회 메서드

--- src/main/java/com/example/m10/domain/project/domain/repository/ProjectRepository.java
+++ src/main/java/com/example/m10/domain/project/domain/repository/ProjectRepository.java
@@ -4,6 +4,10 @@ import com.example.m10.domain.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findAllByOwner_AccountId(String accountId);
 }
