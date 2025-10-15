f8743ddbca0d09b9a31a2172405ab9939d8394ce
sorim0626 <enhld00@gmail.com>
2025-10-15 19:45:35 +0900

    refactor: userFacade 적용

--- src/main/java/com/example/m10/domain/project/service/QueryAllProjectService.java
+++ src/main/java/com/example/m10/domain/project/service/QueryAllProjectService.java
@@ -2,6 +2,8 @@ package com.example.m10.domain.project.service;
 
 import com.example.m10.domain.project.domain.repository.ProjectRepository;
 import com.example.m10.domain.project.presentation.dto.response.QueryAllProjectResponse;
+import com.example.m10.domain.user.domain.User;
+import com.example.m10.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +15,13 @@ import java.util.List;
 public class QueryAllProjectService {
 
     private final ProjectRepository projectRepository;
+    private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
-    public List<QueryAllProjectResponse> getAllProjects(Long accountId) {
-        return projectRepository.findAll()
+    public List<QueryAllProjectResponse> getAllProjects() {
+        User user = userFacade.getCurrentUser();
+
+        return projectRepository.findAllByOwner_AccountId(user.getAccountId())
                 .stream()
                 .map(QueryAllProjectResponse::from)
                 .toList();
